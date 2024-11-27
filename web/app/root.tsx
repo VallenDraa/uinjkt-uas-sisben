badbabbf524d86641ce350cdbf1b72f7795cfc4e
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-11-27 07:42:17 +0700

    feat: add shadcn ui

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -6,7 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-import { ChakraProvider } from "./shared/components/ui/provider";
+import stylesheet from "~/global.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -19,6 +19,7 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -40,9 +41,5 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return (
-    <ChakraProvider>
-      <Outlet />
-    </ChakraProvider>
-  );
+  return <Outlet />;
 }
---
+++ web/app/shared/components/ui/typography.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "~/shared/utils/shadcn";
+
+export const typographyVariants = cva("", {
+  variants: {
+    variant: {
+      h1: "text-4xl font-extrabold tracking-tight lg:text-5xl",
+      h2: "text-2xl font-semibold tracking-tight lg:text-3xl",
+      h3: "text-xl font-semibold tracking-tight lg:text-2xl",
+      h4: "text-lg font-semibold tracking-tight lg:text-xl",
+      lead: "text-lg text-muted-foreground lg:text-xl",
+      large: "text-md font-semibold lg:text-lg",
+      small: "text-sm font-medium leading-none",
+      muted: "text-sm text-muted-foreground",
+      p: "leading-7",
+      blockquote: "mt-6 border-l-2 pl-6 italic",
+      code: "rounded-md bg-muted px-[0.4rem] py-[0.2rem] font-mono text-sm font-semibold",
+      label: "text-sm font-medium leading-none cursor-pointer h-6",
+      error: "text-sm font-medium text-destructive",
+    },
+  },
+  defaultVariants: {
+    variant: "p",
+  },
+});
+
+export type TypographyProps<TElement extends React.ElementType> =
+  React.ComponentPropsWithoutRef<TElement> &
+    VariantProps<typeof typographyVariants> & {
+      className?: string;
+      asChild?: boolean;
+      tag: TElement;
+    };
+
+export const Typography = <TElement extends React.ElementType>({
+  tag,
+  className,
+  variant,
+  asChild = false,
+  ...props
+}: TypographyProps<TElement>) => {
+  const Comp = asChild ? Slot : tag;
+
+  return (
+    <Comp
+      className={cn(typographyVariants({ variant, className }))}
+      {...props}
+    />
+  );
+};
+
+Typography.displayName = "Typography";
