e19d0b917ef4daae15228d6664ea88cb1b012981
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-12-05 10:53:20 +0700

    feat: add dependencies for PWA support

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,34 @@
+// TODO: Configure PWA
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+// import { RemixVitePWA, RemixPWAOptions } from "@vite-pwa/remix";
+
+// const { RemixVitePWAPlugin, RemixPWAPreset } = RemixVitePWA();
+
+// const manifestForPlugin: RemixPWAOptions = {
+//   registerType: "autoUpdate",
+//   minify: true,
+//   workbox: { clientsClaim: true, skipWaiting: true },
+//   devOptions: { enabled: true },
+//   includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+//   pwaAssets: {
+//     config: true,
+//   },
+//   manifest: {
+//     name: "SeeBaby",
+//     lang: "id",
+//     short_name: "SeeBaby",
+//     description:
+//       "no BS all-in-one solution untuk monitoring dan perawatan bayi anda.",
+//     background_color: "#ffffff",
+//     theme_color: "#0284c7",
+//     display: "standalone",
+//     scope: "/",
+//     start_url: "/",
+//     orientation: "portrait",
+//   },
+// };
 
 declare module "@remix-run/node" {
   interface Future {
@@ -11,6 +39,7 @@ declare module "@remix-run/node" {
 export default defineConfig({
   plugins: [
     remix({
+      // presets: [RemixPWAPreset],
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
@@ -20,5 +49,6 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    // RemixVitePWAPlugin(manifestForPlugin),
   ],
 });
