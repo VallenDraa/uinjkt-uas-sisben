0a25f1037bf3feea42efb59c8c46b701ac0315f1
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-12-05 19:28:14 +0700

    feat: allow all hosts in Django settings

--- server/server_app/server_app/settings.py
+++ server/server_app/server_app/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+ALLOWED_HOSTS = ["*"]
+
 CORS_ALLOW_ALL_ORIGINS = (
     True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 )
